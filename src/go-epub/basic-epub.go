beebf7c1cae2c98370184568c44cc0e199948c50
Thomas Jespersen <laumann.thomas@gmail.com>
2013-02-01 13:57:15 +0100

    Cleaning...

--- src/go-epub/basic-epub.go
+++ src/go-epub/basic-epub.go
@@ -13,18 +13,10 @@ type Book struct {
 }
 
 
-//var autoClosed = 
-
 func main() {
 	opf := exampleOpfPackage()
-	fmt.Printf("%#v\n", opf)
 
 	xml.HTMLAutoClose = []string{ "item", "itemref" } 
-//	for _, tag := range autoClosed {
-//		xml.HTMLAutoClose = append(xml.HTMLAutoClose, tag)
-//	}
-
-	fmt.Printf("HTMLAutoClose: ", xml.HTMLAutoClose)
 
 	opfXml, err := xml.MarshalIndent(opf, "", "  ")
 	if err != nil {
