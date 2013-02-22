0966f7d67cf224391dffd76556c56c83bde64150
Thomas Jespersen <laumann.thomas@gmail.com>
2013-02-22 14:57:23 +0100

    Move pathutil -> util/paths

--- src/pathutildemo/pathutildemo.go
+++ src/pathutildemo/pathutildemo.go
@@ -1,11 +1,26 @@
 package main
 
-import "pathutil"
+import "util/paths"
 import "fmt"
 
+var (
+	p = paths.Get("~/Downloads")
+	op = paths.Get("~")
+	oop = paths.Get("~/Downloads")
+	opf = paths.Get("~/Pictures")
+)
+
 func main() {
-	p := pathutil.Get("~/Downloads")
+	p := paths.Get("~/Downloads")
 	fmt.Println(p)
 	fmt.Println(p.ToAbs())
-	fmt.Printf("%#v\n", p)
+	fmt.Println(p.NameCount())
+	fmt.Println(p.Root())
+	fmt.Println(p.IsAbs())
+
+	fmt.Println("\n")
+	fmt.Printf("p.StartsWith(%s): %t\n", op, p.StartsWith(op))
+	fmt.Printf("p.StartsWith(%s): %t\n", oop, p.StartsWith(oop))
+	fmt.Printf("p.StartsWith(%s): %t\n", opf, p.StartsWith(opf))
+
 }
